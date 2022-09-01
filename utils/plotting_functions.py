8429ddfdfa54ca1bc3f2c5cafbe4abe573ca4b4d
Marcos Tapia Costa <marcos@Marcoss-MacBook-Pro.local>
2022-09-01 19:34:31 +0200

    Added QQ Plotting and more math functions

--- utils/plotting_functions.py
+++ utils/plotting_functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import numbers
 
 def qqplot(x, y, quantiles=None, interpolation='nearest', ax=None, rug=False, 
            rug_length=0.05, rug_kwargs=None, font_size=14, title_size=14, **kwargs):
@@ -42,22 +42,22 @@ def qqplot(x, y, quantiles=None, interpolation='nearest', ax=None, rug=False,
         the q-q plot.
     """
     plt.rc('font', size=font_size)
-    plt.rc('axes', titlesize=title_size) 
-    # Get current axes if none are provided
+    plt.rc('axes', titlesize=title_size)
+    x1 = x
+    y1 = y
+
     if ax is None:
         ax = plt.gca()
-
     if quantiles is None:
-        quantiles = min(len(x), len(y))
+        quantiles = min(len(x1), len(y1))
 
     # Compute quantiles of the two samples
     if isinstance(quantiles, numbers.Integral):
         quantiles = np.linspace(start=0, stop=1, num=int(quantiles))
     else:
         quantiles = np.atleast_1d(np.sort(quantiles))
-    x_quantiles = np.quantile(x, quantiles, interpolation=interpolation)
-    y_quantiles = np.quantile(y, quantiles, interpolation=interpolation)
-
+    x_quantiles1 = np.quantile(x1, quantiles, interpolation=interpolation)
+    y_quantiles1 = np.quantile(y1, quantiles, interpolation=interpolation)
     # Draw the rug plots if requested
     if rug:
         # Default rug plot settings
@@ -70,13 +70,22 @@ def qqplot(x, y, quantiles=None, interpolation='nearest', ax=None, rug=False,
             rug_y_params.update(rug_kwargs)
 
         # Draw the rug plots
-        for point in x:
+        for point in x1:
             ax.axvline(point, **rug_x_params)
-        for point in y:
+        for point in y1:
             ax.axhline(point, **rug_y_params)
 
-    # Draw the q-q plot
-    ax.scatter(x_quantiles, y_quantiles, **kwargs)
+    # Draw the q-q plot and compare with y = x
+    ax.scatter(x_quantiles1, y_quantiles1, c="black", label="Q-Q plot", **kwargs)
+    lims = [
+        np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
+        np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
+    ]
+    # now plot both limits against each other
+    ax.plot(lims, lims, 'k-', alpha=0.75, zorder=0, label="45 degree line")
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.legend()
 
 
 def histogramplot(rvs, pdf_vals, num_bins = 100, xlabel="", ylabel="", plottitle="", plottlabel="", ax=None):
